323651e977f2932bdab965ebdcb4d19982805a65
Johan <01johanalba@gmail.com>
2026-04-05 22:37:05 -0500

    first commit

---
+++ products/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+
+@admin.register(Product)
+class ProducAdmin(admin.ModelAdmin):
+    list_display = ['id','name','price','stock','category']
+    
+
+# Register your models here.
